11943474a175b0cd375eb7d5a22d42213ecf31ad
Timothy Southwick <NickNackGus@gmail.com>
2026-05-09 18:40:01 -0400

    Mainhand/offhand shows lines of text instead of a full item (some special values not included)

--- network-chat/src/main/java/com/playmonumenta/networkchat/inlinereplacements/ItemHoverReplacement.java
+++ network-chat/src/main/java/com/playmonumenta/networkchat/inlinereplacements/ItemHoverReplacement.java
@@ -1,12 +1,29 @@
 package com.playmonumenta.networkchat.inlinereplacements;
 
+import com.destroystokyo.paper.Namespaced;
+import com.google.common.collect.Multimap;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.JoinConfiguration;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.Material;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeModifier;
+import org.bukkit.command.CommandSender;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.BookMeta;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.intellij.lang.annotations.RegExp;
 
 public class ItemHoverReplacement extends InlineReplacement {
@@ -40,21 +57,141 @@ public class ItemHoverReplacement extends InlineReplacement {
 		super("Item Hover",
 			"(?<=^|[^\\\\])<(mainhand|offhand)>",
 			"itemhover");
-		addHandler(mMainhandRegex, sender -> {
-			if (sender instanceof Player player
-				&& !BANNED_MATERIALS.contains(player.getInventory().getItemInMainHand().getType())) {
-				ItemStack item = player.getInventory().getItemInMainHand();
-				return item.displayName().hoverEvent(item);
-			}
-			return Component.text("<mainhand>");
-		});
-		addHandler(mOffhandRegex, sender -> {
-			if (sender instanceof Player player
-				&& !BANNED_MATERIALS.contains(player.getInventory().getItemInOffHand().getType())) {
-				ItemStack item = player.getInventory().getItemInOffHand();
-				return item.displayName().hoverEvent(item);
-			}
-			return Component.text("<offhand>");
-		});
+		addHandler(mMainhandRegex, sender -> hoverComponent(sender, true));
+		addHandler(mOffhandRegex, sender -> hoverComponent(sender, false));
+	}
+
+	public static Component hoverComponent(CommandSender sender, boolean isMainHand) {
+		if (sender instanceof Player player) {
+		    ItemStack item = isMainHand ? player.getInventory().getItemInMainHand() : player.getInventory().getItemInOffHand();
+			if (!BANNED_MATERIALS.contains(player.getInventory().getItemInOffHand().getType())) {
+				List<Component> lines = new ArrayList<>();
+				lines.add(item.displayName());
+				ItemMeta meta = item.getItemMeta();
+
+				if (!meta.hasItemFlag(ItemFlag.HIDE_POTION_EFFECTS)) {
+					if (meta instanceof BookMeta bookMeta) {
+						Component author = bookMeta.author();
+						if (bookMeta.hasAuthor() && author != null) {
+							lines.add(Component.translatable("book.byAuthor", List.of(author))
+								.color(NamedTextColor.GRAY)
+								.decoration(TextDecoration.ITALIC, false));
+						}
+						BookMeta.Generation generation = bookMeta.getGeneration();
+						if (bookMeta.hasGeneration() && generation != null) {
+							int generationId = generation.ordinal();
+							lines.add(Component.translatable(String.format("book.generation.%d", generationId), NamedTextColor.GRAY)
+								.decoration(TextDecoration.ITALIC, false));
+						}
+					}
+				}
+
+				if (!meta.hasItemFlag(ItemFlag.HIDE_ENCHANTS)) {
+					for (Map.Entry<Enchantment, Integer> entry : meta.getEnchants().entrySet()) {
+						lines.add(entry.getKey().displayName(entry.getValue())
+							.color(NamedTextColor.GRAY)
+							.decoration(TextDecoration.ITALIC, false));
+					}
+				}
+
+				List<Component> lore = meta.lore();
+				if (lore != null) {
+					lines.addAll(lore);
+				}
+
+				Multimap<Attribute, AttributeModifier> attributeModifiers = meta.getAttributeModifiers();
+				if (!meta.hasItemFlag(ItemFlag.HIDE_ATTRIBUTES) && attributeModifiers != null) {
+					Map<EquipmentSlot, List<AttributeAndModifier>> modifiersBySlot = new HashMap<>();
+
+					for (Map.Entry<Attribute, AttributeModifier> entry : attributeModifiers.entries()) {
+						Attribute attribute = entry.getKey();
+						AttributeModifier modifier = entry.getValue();
+						modifiersBySlot.computeIfAbsent(modifier.getSlot(), k -> new ArrayList<>())
+							.add(new AttributeAndModifier(attribute, modifier));
+					}
+
+					List<AttributeAndModifier> onAllSlots = modifiersBySlot.getOrDefault(null, List.of());
+
+					for (EquipmentSlot slot : EquipmentSlot.values()) {
+						List<AttributeAndModifier> slotAttributes = new ArrayList<>(onAllSlots);
+						slotAttributes.addAll(modifiersBySlot.getOrDefault(slot, List.of()));
+						if (slotAttributes.isEmpty()) {
+							continue;
+						}
+
+						String whenInSlotTranslatable = switch (slot) {
+							case HAND -> "item.modifiers.mainhand";
+							case OFF_HAND -> "item.modifiers.offhand";
+							case FEET -> "item.modifiers.feet";
+							case LEGS -> "item.modifiers.legs";
+							case CHEST -> "item.modifiers.chest";
+							case HEAD -> "item.modifiers.head";
+						};
+
+						lines.add(Component.empty());
+						lines.add(Component.translatable(whenInSlotTranslatable, NamedTextColor.GRAY)
+							.decoration(TextDecoration.ITALIC, false));
+						for (AttributeAndModifier attributeAndModifier : slotAttributes) {
+							Attribute attribute = attributeAndModifier.attribute;
+
+							AttributeModifier modifier = attributeAndModifier.modifier;
+							double amount = modifier.getAmount();
+							boolean isNegative = amount < 0;
+							int operationId = modifier.getOperation().ordinal();
+							String modifierTranslatable = String.format(
+								"attribute.modifier.%s.%d",
+								isNegative ? "take" : "plus",
+								operationId
+							);
+							List<Component> modifierTranslatableArguments = List.of(
+								Component.text(isNegative ? -amount : amount),
+								Component.translatable(attribute)
+							);
+
+							lines.add(Component.translatable(modifierTranslatable, modifierTranslatableArguments)
+								.color(NamedTextColor.BLUE)
+								.decoration(TextDecoration.ITALIC, false));
+						}
+					}
+				}
+
+				if (!meta.hasItemFlag(ItemFlag.HIDE_UNBREAKABLE) && meta.isUnbreakable()) {
+					lines.add(Component.translatable("item.unbreakable", NamedTextColor.BLUE)
+						.decoration(TextDecoration.ITALIC, false));
+				}
+
+				Set<Namespaced> destroyables = meta.getDestroyableKeys();
+				if (!meta.hasItemFlag(ItemFlag.HIDE_DESTROYS) && !destroyables.isEmpty()) {
+					lines.add(Component.empty());
+					lines.add(Component.translatable("item.canBreak", NamedTextColor.GRAY)
+						.decoration(TextDecoration.ITALIC, false));
+					for (Namespaced destoryable : destroyables) {
+						lines.add(Component.translatable(
+							String.format("block.%s.%s", destoryable.getNamespace(), destoryable.getKey()),
+							NamedTextColor.DARK_GRAY
+						).decoration(TextDecoration.ITALIC, false));
+					}
+				}
+
+				Set<Namespaced> placeables = meta.getPlaceableKeys();
+				if (!meta.hasItemFlag(ItemFlag.HIDE_DESTROYS) && !placeables.isEmpty()) {
+					lines.add(Component.empty());
+					lines.add(Component.translatable("item.canPlace", NamedTextColor.GRAY)
+						.decoration(TextDecoration.ITALIC, false));
+					for (Namespaced placeable : placeables) {
+						lines.add(Component.translatable(
+							String.format("block.%s.%s", placeable.getNamespace(), placeable.getKey()),
+							NamedTextColor.DARK_GRAY
+						).decoration(TextDecoration.ITALIC, false));
+					}
+				}
+
+				return item.displayName().hoverEvent(Component.join(JoinConfiguration.newlines(), lines));
+		    }
+		}
+		return Component.text(isMainHand ? "<mainhand>" : "<offhand>");
+	}
+
+	public record AttributeAndModifier(Attribute attribute, AttributeModifier modifier) {
 	}
 }
