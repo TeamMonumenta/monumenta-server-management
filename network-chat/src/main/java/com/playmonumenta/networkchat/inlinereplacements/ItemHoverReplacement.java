351c732e56be64af51a08425b523303ccbdd9a26
Ascynx <miaoum2128@gmail.com>
2026-05-18 11:17:44 +0200

    Fix error when using item hover on empty slot

--- network-chat/src/main/java/com/playmonumenta/networkchat/inlinereplacements/ItemHoverReplacement.java
+++ network-chat/src/main/java/com/playmonumenta/networkchat/inlinereplacements/ItemHoverReplacement.java
@@ -67,113 +67,116 @@ public class ItemHoverReplacement extends InlineReplacement {
 			if (!BANNED_MATERIALS.contains(player.getInventory().getItemInOffHand().getType())) {
 				List<Component> lines = new ArrayList<>();
 				lines.add(item.displayName());
-				ItemMeta meta = item.getItemMeta();
 
-				if (!meta.hasItemFlag(ItemFlag.HIDE_POTION_EFFECTS)) {
-					if (meta instanceof BookMeta bookMeta) {
-						Component author = bookMeta.author();
-						if (bookMeta.hasAuthor() && author != null) {
-							lines.add(Component.translatable("book.byAuthor", List.of(author))
-								.color(NamedTextColor.GRAY));
-						}
-						BookMeta.Generation generation = bookMeta.getGeneration();
-						if (bookMeta.hasGeneration() && generation != null) {
-							int generationId = generation.ordinal();
-							lines.add(Component.translatable(String.format("book.generation.%d", generationId), NamedTextColor.GRAY));
+
+				if (item.hasItemMeta()) {
+					ItemMeta meta = item.getItemMeta();
+					if (!meta.hasItemFlag(ItemFlag.HIDE_POTION_EFFECTS)) {
+						if (meta instanceof BookMeta bookMeta) {
+							Component author = bookMeta.author();
+							if (bookMeta.hasAuthor() && author != null) {
+								lines.add(Component.translatable("book.byAuthor", List.of(author))
+									.color(NamedTextColor.GRAY));
+							}
+							BookMeta.Generation generation = bookMeta.getGeneration();
+							if (bookMeta.hasGeneration() && generation != null) {
+								int generationId = generation.ordinal();
+								lines.add(Component.translatable(String.format("book.generation.%d", generationId), NamedTextColor.GRAY));
+							}
 						}
 					}
-				}
 
-				if (!meta.hasItemFlag(ItemFlag.HIDE_ENCHANTS)) {
-					for (Map.Entry<Enchantment, Integer> entry : meta.getEnchants().entrySet()) {
-						lines.add(entry.getKey().displayName(entry.getValue()).color(NamedTextColor.GRAY));
-					}
-				}
-
-				List<Component> lore = meta.lore();
-				if (lore != null) {
-					lines.addAll(lore);
-				}
-
-				Multimap<Attribute, AttributeModifier> attributeModifiers = meta.getAttributeModifiers();
-				if (!meta.hasItemFlag(ItemFlag.HIDE_ATTRIBUTES) && attributeModifiers != null) {
-					Map<EquipmentSlot, List<AttributeAndModifier>> modifiersBySlot = new HashMap<>();
-
-					for (Map.Entry<Attribute, AttributeModifier> entry : attributeModifiers.entries()) {
-						Attribute attribute = entry.getKey();
-						AttributeModifier modifier = entry.getValue();
-						modifiersBySlot.computeIfAbsent(modifier.getSlot(), k -> new ArrayList<>())
-							.add(new AttributeAndModifier(attribute, modifier));
+					if (!meta.hasItemFlag(ItemFlag.HIDE_ENCHANTS)) {
+						for (Map.Entry<Enchantment, Integer> entry : meta.getEnchants().entrySet()) {
+							lines.add(entry.getKey().displayName(entry.getValue()).color(NamedTextColor.GRAY));
+						}
 					}
 
-					List<AttributeAndModifier> onAllSlots = modifiersBySlot.getOrDefault(null, List.of());
+					List<Component> lore = meta.lore();
+					if (lore != null) {
+						lines.addAll(lore);
+					}
 
-					for (EquipmentSlot slot : EquipmentSlot.values()) {
-						List<AttributeAndModifier> slotAttributes = new ArrayList<>(onAllSlots);
-						slotAttributes.addAll(modifiersBySlot.getOrDefault(slot, List.of()));
-						if (slotAttributes.isEmpty()) {
-							continue;
+					Multimap<Attribute, AttributeModifier> attributeModifiers = meta.getAttributeModifiers();
+					if (!meta.hasItemFlag(ItemFlag.HIDE_ATTRIBUTES) && attributeModifiers != null) {
+						Map<EquipmentSlot, List<AttributeAndModifier>> modifiersBySlot = new HashMap<>();
+
+						for (Map.Entry<Attribute, AttributeModifier> entry : attributeModifiers.entries()) {
+							Attribute attribute = entry.getKey();
+							AttributeModifier modifier = entry.getValue();
+							modifiersBySlot.computeIfAbsent(modifier.getSlot(), k -> new ArrayList<>())
+								.add(new AttributeAndModifier(attribute, modifier));
 						}
 
-						String whenInSlotTranslatable = switch (slot) {
-							case HAND -> "item.modifiers.mainhand";
-							case OFF_HAND -> "item.modifiers.offhand";
-							case FEET -> "item.modifiers.feet";
-							case LEGS -> "item.modifiers.legs";
-							case CHEST -> "item.modifiers.chest";
-							case HEAD -> "item.modifiers.head";
-						};
+						List<AttributeAndModifier> onAllSlots = modifiersBySlot.getOrDefault(null, List.of());
 
+						for (EquipmentSlot slot : EquipmentSlot.values()) {
+							List<AttributeAndModifier> slotAttributes = new ArrayList<>(onAllSlots);
+							slotAttributes.addAll(modifiersBySlot.getOrDefault(slot, List.of()));
+							if (slotAttributes.isEmpty()) {
+								continue;
+							}
+
+							String whenInSlotTranslatable = switch (slot) {
+								case HAND -> "item.modifiers.mainhand";
+								case OFF_HAND -> "item.modifiers.offhand";
+								case FEET -> "item.modifiers.feet";
+								case LEGS -> "item.modifiers.legs";
+								case CHEST -> "item.modifiers.chest";
+								case HEAD -> "item.modifiers.head";
+							};
+
+							lines.add(Component.empty());
+							lines.add(Component.translatable(whenInSlotTranslatable, NamedTextColor.GRAY));
+							for (AttributeAndModifier attributeAndModifier : slotAttributes) {
+								Attribute attribute = attributeAndModifier.attribute;
+
+								AttributeModifier modifier = attributeAndModifier.modifier;
+								double amount = modifier.getAmount();
+								boolean isNegative = amount < 0;
+								int operationId = modifier.getOperation().ordinal();
+								String modifierTranslatable = String.format(
+									"attribute.modifier.%s.%d",
+									isNegative ? "take" : "plus",
+									operationId
+								);
+								List<Component> modifierTranslatableArguments = List.of(
+									Component.text(isNegative ? -amount : amount),
+									Component.translatable(attribute)
+								);
+
+								lines.add(Component.translatable(modifierTranslatable, modifierTranslatableArguments)
+									.color(NamedTextColor.BLUE));
+							}
+						}
+					}
+
+					if (!meta.hasItemFlag(ItemFlag.HIDE_UNBREAKABLE) && meta.isUnbreakable()) {
+						lines.add(Component.translatable("item.unbreakable", NamedTextColor.BLUE));
+					}
+
+					Set<Namespaced> destroyables = meta.getDestroyableKeys();
+					if (!meta.hasItemFlag(ItemFlag.HIDE_DESTROYS) && !destroyables.isEmpty()) {
 						lines.add(Component.empty());
-						lines.add(Component.translatable(whenInSlotTranslatable, NamedTextColor.GRAY));
-						for (AttributeAndModifier attributeAndModifier : slotAttributes) {
-							Attribute attribute = attributeAndModifier.attribute;
-
-							AttributeModifier modifier = attributeAndModifier.modifier;
-							double amount = modifier.getAmount();
-							boolean isNegative = amount < 0;
-							int operationId = modifier.getOperation().ordinal();
-							String modifierTranslatable = String.format(
-								"attribute.modifier.%s.%d",
-								isNegative ? "take" : "plus",
-								operationId
-							);
-							List<Component> modifierTranslatableArguments = List.of(
-								Component.text(isNegative ? -amount : amount),
-								Component.translatable(attribute)
-							);
-
-							lines.add(Component.translatable(modifierTranslatable, modifierTranslatableArguments)
-								.color(NamedTextColor.BLUE));
+						lines.add(Component.translatable("item.canBreak", NamedTextColor.GRAY));
+						for (Namespaced destoryable : destroyables) {
+							lines.add(Component.translatable(
+								String.format("block.%s.%s", destoryable.getNamespace(), destoryable.getKey()),
+								NamedTextColor.DARK_GRAY
+							));
 						}
 					}
-				}
 
-				if (!meta.hasItemFlag(ItemFlag.HIDE_UNBREAKABLE) && meta.isUnbreakable()) {
-					lines.add(Component.translatable("item.unbreakable", NamedTextColor.BLUE));
-				}
-
-				Set<Namespaced> destroyables = meta.getDestroyableKeys();
-				if (!meta.hasItemFlag(ItemFlag.HIDE_DESTROYS) && !destroyables.isEmpty()) {
-					lines.add(Component.empty());
-					lines.add(Component.translatable("item.canBreak", NamedTextColor.GRAY));
-					for (Namespaced destoryable : destroyables) {
-						lines.add(Component.translatable(
-							String.format("block.%s.%s", destoryable.getNamespace(), destoryable.getKey()),
-							NamedTextColor.DARK_GRAY
-						));
-					}
-				}
-
-				Set<Namespaced> placeables = meta.getPlaceableKeys();
-				if (!meta.hasItemFlag(ItemFlag.HIDE_DESTROYS) && !placeables.isEmpty()) {
-					lines.add(Component.empty());
-					lines.add(Component.translatable("item.canPlace", NamedTextColor.GRAY));
-					for (Namespaced placeable : placeables) {
-						lines.add(Component.translatable(
-							String.format("block.%s.%s", placeable.getNamespace(), placeable.getKey()),
-							NamedTextColor.DARK_GRAY
-						));
+					Set<Namespaced> placeables = meta.getPlaceableKeys();
+					if (!meta.hasItemFlag(ItemFlag.HIDE_DESTROYS) && !placeables.isEmpty()) {
+						lines.add(Component.empty());
+						lines.add(Component.translatable("item.canPlace", NamedTextColor.GRAY));
+						for (Namespaced placeable : placeables) {
+							lines.add(Component.translatable(
+								String.format("block.%s.%s", placeable.getNamespace(), placeable.getKey()),
+								NamedTextColor.DARK_GRAY
+							));
+						}
 					}
 				}
 
