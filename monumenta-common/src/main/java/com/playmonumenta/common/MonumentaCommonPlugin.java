e4b212f96a70d053a56f5481d3c1915b3f836570
Timothy Southwick <NickNackGus@gmail.com>
2026-07-21 01:07:16 -0400

    Implement more common utils.MMLog instance in common library, as well as getInstance()

--- monumenta-common/src/main/java/com/playmonumenta/common/MonumentaCommonPlugin.java
+++ monumenta-common/src/main/java/com/playmonumenta/common/MonumentaCommonPlugin.java
@@ -1,17 +1,31 @@
 package com.playmonumenta.common;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.Nullable;
 
 public class MonumentaCommonPlugin extends JavaPlugin {
+	private static @Nullable MonumentaCommonPlugin INSTANCE = null;
+
 	@Override
 	public void onEnable() {
-		MMLog log = new MMLog(getName());
-		MMLogPaper.registerCommand(log);
+		com.playmonumenta.common.utils.MMLog.init(getName());
+		MMLogPaper.registerCommand(com.playmonumenta.common.utils.MMLog.getLog());
+
+		INSTANCE = this;
 		getLogger().info("MonumentaCommon enabled");
 	}
 
 	@Override
 	public void onDisable() {
+		INSTANCE = null;
 		getLogger().info("MonumentaCommon disabled");
+	}
+
+	public static MonumentaCommonPlugin getInstance() {
+		final MonumentaCommonPlugin instance = INSTANCE;
+		if (instance == null) {
+			throw new RuntimeException("Attempted to access MonumentaCommonPlugin plugin before it loaded.");
+		}
+		return instance;
 	}
 }
