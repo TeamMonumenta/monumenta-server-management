2c5312bb16f26063502c32180ac7ff671121080b
ash <ashenphyxation@gmail.com>
2026-09-07 01:48:54 +0800

    ash/request-28 (#72)
    ---------
    
    Co-authored-by: Timothy Southwick <NickNackGus@gmail.com>

--- monumenta-common/src/main/java/com/playmonumenta/common/MonumentaCommonPlugin.java
+++ monumenta-common/src/main/java/com/playmonumenta/common/MonumentaCommonPlugin.java
@@ -1,5 +1,10 @@
 package com.playmonumenta.common;
 
+import com.playmonumenta.common.commands.GetDateCommand;
+import com.playmonumenta.common.commands.RefreshTimeCommand;
+import com.playmonumenta.common.commands.TimeWarpCommand;
+import com.playmonumenta.common.managers.TimeWarpManager;
+import com.playmonumenta.common.utils.DateUtils;
 import com.playmonumenta.common.zones.ZoneManager;
 import com.playmonumenta.common.zones.commands.DebugZones;
 import com.playmonumenta.common.zones.commands.ShowZones;
@@ -15,17 +20,44 @@ import org.jetbrains.annotations.Nullable;
 
 public class MonumentaCommonPlugin extends JavaPlugin {
 	private static @Nullable MonumentaCommonPlugin INSTANCE = null;
+	public static final boolean ENABLE_TIME_WARP;
 
 	public boolean mShowZonesDynmap = false;
 	public boolean mFallbackZoneLookup = false;
 
 	public @MonotonicNonNull ZoneManager mZoneManager;
 
+	static {
+		/*
+		 * Reads the environment variable MONUMENTA_ENABLE_TIME_WARP to determine if /timewarp is allowed
+		 * If environment variable is not set or 0, disabled. If set and nonzero, enabled.
+		 */
+		String envEnableTimeWarp = System.getenv("MONUMENTA_ENABLE_TIME_WARP");
+		if (envEnableTimeWarp == null || envEnableTimeWarp.isEmpty()) {
+			ENABLE_TIME_WARP = false;
+		} else {
+			boolean val;
+			try {
+				val = Integer.parseInt(envEnableTimeWarp) != 0;
+			} catch (Exception ex) {
+				val = false;
+			}
+			ENABLE_TIME_WARP = val;
+		}
+	}
+
 	@Override
 	public void onLoad() {
+		// Zone registration
 		DebugZones.register();
 		TestZone.register();
 		ShowZones.register(this);
+
+		// Time registration
+		TimeWarpCommand.register();
+		GetDateCommand.register();
+		RefreshTimeCommand.register();
+		DateUtils.refreshTime();
 	}
 
 	@Override
@@ -40,6 +72,8 @@ public class MonumentaCommonPlugin extends JavaPlugin {
 
 		mZoneManager = ZoneManager.getInstance();
 		mZoneManager.doReload(true);
+
+		TimeWarpManager.load();
 
 		/* Load the config 1 tick later to let other plugins load */
 		new BukkitRunnable() {
---
+++ monumenta-common/src/main/java/com/playmonumenta/common/managers/TimeWarpManager.java
@@ -0,0 +1,80 @@
+package com.playmonumenta.common.managers;
+
+import com.google.gson.JsonObject;
+import com.playmonumenta.common.MonumentaCommonPlugin;
+import com.playmonumenta.common.event.RefreshTimeEvent;
+import com.playmonumenta.common.utils.DateUtils;
+import com.playmonumenta.common.utils.FileUtils;
+import com.playmonumenta.common.utils.MMLog;
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.temporal.TemporalUnit;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextDecoration;
+import org.bukkit.Bukkit;
+
+public class TimeWarpManager {
+	public static String CONFIG_NAME = "time_warp.json";
+	public static final String TIMEWARP_OFFSET_PATH = "timewarp_seconds_offset";
+	private static long mSecondOffset = 0;
+
+	public static void reset() {
+		set(0);
+	}
+
+	public static void add(long amount, TemporalUnit unit) {
+		set(DateUtils.localDateTime().plus(amount, unit));
+	}
+
+	public static void set(LocalDateTime desiredUtcTime) {
+		set(desiredUtcTime.toEpochSecond((ZoneOffset) DateUtils.TIMEZONE)
+			- DateUtils.trueLocalDateTime().toEpochSecond((ZoneOffset) DateUtils.TIMEZONE));
+	}
+
+	public static void set(long offsetSeconds) {
+		mSecondOffset = offsetSeconds;
+		save();
+	}
+
+	public static long get() {
+		return mSecondOffset;
+	}
+
+	public static void load() {
+		if (!MonumentaCommonPlugin.ENABLE_TIME_WARP) {
+			reset();
+			return;
+		}
+
+		File configFile = getConfigFile();
+		try {
+			JsonObject config = FileUtils.readJson(configFile.getAbsolutePath());
+			set(config.get(TIMEWARP_OFFSET_PATH).getAsLong());
+		} catch (Exception e) {
+			MMLog.warning("Failed to load " + CONFIG_NAME + ": " + e.getMessage());
+			reset();
+		}
+	}
+
+	private static void save() {
+		// Update real-time-based plugin code
+		RefreshTimeEvent refreshTimeEvent = new RefreshTimeEvent();
+		refreshTimeEvent.callEvent();
+		Bukkit.getServer().sendMessage(Component.text("The hands of time drift, its sands fly ever swift...", NamedTextColor.DARK_AQUA, TextDecoration.ITALIC));
+
+		JsonObject config = new JsonObject();
+		config.addProperty("offset_seconds", mSecondOffset);
+		try {
+			FileUtils.writeJson(getConfigFile().getAbsolutePath(), config);
+		} catch (IOException e) {
+			MMLog.warning("Failed to save " + CONFIG_NAME + ": " + e.getMessage());
+		}
+	}
+
+	private static File getConfigFile() {
+		return new File(MonumentaCommonPlugin.getInstance().getDataFolder(), CONFIG_NAME);
+	}
+}
