0b98459a8c39a7af44d26419935296347826fb41
NobodyPi <officialnobodypi@gmail.com>
2026-09-11 13:37:47 -1000

    Handle no shard data case

--- redis-sync/plugin/src/main/java/com/playmonumenta/redissync/MonumentaRedisSyncAPI.java
+++ redis-sync/plugin/src/main/java/com/playmonumenta/redissync/MonumentaRedisSyncAPI.java
@@ -728,23 +728,21 @@ public class MonumentaRedisSyncAPI {
 				MMLog.severe("Failed to set location on other shard for player=" + player.getName(), ex);
 			}
 
-			if (shardData == null || shardData.isEmpty()) {
-				// If we want to be able to set players' world data on shards they have never been to before,
-				// this needs to change. Instead, we would need to set a default shard data (empty json?)
-				MMLog.info("Attempted to set data for player=" + player.getName() + " on shard=" + shard + " they have never visited before (nothing occured).");
-				return;
-			}
-
 			final JsonObject worldShardDataJson;
-			/* Look up in the shard data first the "world" part - data from this world about where the player should be */
 			String worldKey = getRedisPerShardDataWorldKey(worldName);
-			String worldShardData = shardData.get(worldKey);
-			if (worldShardData == null || worldShardData.isEmpty()) {
-				MMLog.trace("No world shard data for player '" + player.getName() + "', using default");
+			if (shardData == null || shardData.isEmpty()) {
+				MMLog.trace("No shard data for player '" + player.getName() + "', using default");
 				worldShardDataJson = new JsonObject();
 			} else {
-				MMLog.trace("Found world shard data for player '" + player.getName() + "': '" + worldShardData + "'");
-				worldShardDataJson = new Gson().fromJson(worldShardData, JsonObject.class);
+				/* Look up in the shard data first the "world" part - data from this world about where the player should be */
+				String worldShardData = shardData.get(worldKey);
+				if (worldShardData == null || worldShardData.isEmpty()) {
+					MMLog.trace("No world shard data for player '" + player.getName() + "', using default");
+					worldShardDataJson = new JsonObject();
+				} else {
+					MMLog.trace("Found world shard data for player '" + player.getName() + "': '" + worldShardData + "'");
+					worldShardDataJson = new Gson().fromJson(worldShardData, JsonObject.class);
+				}
 			}
 
 			JsonArray pos = new JsonArray();
