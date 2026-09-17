63584a3fccc168b698c79b0beb29dd13edf1cbd6
Timothy Southwick <NickNackGus@gmail.com>
2026-09-17 01:33:51 -0400

    Fixed exception enabling MonumentaCommon due to DateUtils

--- monumenta-common/src/main/java/com/playmonumenta/common/MonumentaCommonPlugin.java
+++ monumenta-common/src/main/java/com/playmonumenta/common/MonumentaCommonPlugin.java
@@ -57,7 +57,6 @@ public class MonumentaCommonPlugin extends JavaPlugin {
 		TimeWarpCommand.register();
 		GetDateCommand.register();
 		RefreshTimeCommand.register();
-		DateUtils.refreshTime();
 	}
 
 	@Override
@@ -66,6 +65,7 @@ public class MonumentaCommonPlugin extends JavaPlugin {
 		MMLogPaper.registerCommand(com.playmonumenta.common.utils.MMLog.getLog());
 
 		INSTANCE = this;
+		DateUtils.refreshTime();
 		PluginManager manager = getServer().getPluginManager();
 		manager.registerEvents(new RedisSyncListener(), this);
 		manager.registerEvents(new WorldListener(this), this);
